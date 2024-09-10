ee7f722c4d806d73c71fcfaffde76251246d8695
Dhairya Thakkar <dhairyathakkar25@gmail.com>
2024-09-10 10:22:20 -0400

    complete ex.3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    string_array = string.split('')
+    string_array.reverse()
+    reverse_str = string_array.join("")
+   
+    return reverse_str
 
 };
 
 // Do not edit below this line
+
 module.exports = reverseString;
