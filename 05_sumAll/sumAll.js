4ba4a884dbd47d34ed173e4766c4c7bdeff34ca8
Dhairya Thakkar <dhairyathakkar25@gmail.com>
2024-09-10 11:03:01 -0400

    complete ex.5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || a < 0 || !Number.isInteger(b) || b < 0) {
+        return "ERROR"
+    }
+    max = Math.max(a,b)
+    min = Math.min(a,b)
+    sum = 0
+    for (let i = min; i<= max; i++) {
+        sum += i
+    }
 
+    return sum
 };
 
 // Do not edit below this line
