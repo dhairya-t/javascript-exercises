8fb55a1231593e0641370bba0d6716a6c74787bb
Dhairya Thakkar <dhairyathakkar25@gmail.com>
2024-09-10 10:52:54 -0400

    complete ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...remove_items) {
+    for (const item of remove_items) {
+        while (list.includes(item)) {
+            if (list.indexOf(item) !== -1) {
+                list.splice(list.indexOf(item), 1)
+            }
+        }
+
+
+    }
+
+    return list
+
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
