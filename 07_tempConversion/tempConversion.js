d38c75b61bbc6b8563d49195a05f2a1fec88e8d6
Dhairya Thakkar <dhairyathakkar25@gmail.com>
2024-09-10 11:26:26 -0400

    ex.7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  celsius = Math.round((((degree - 32) * (5/9)))*10)/10
+  return celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  fahrenheit = Math.round((((degree * (9/5)) + 32))*10)/10
+  return fahrenheit
 };
 
 // Do not edit below this line
